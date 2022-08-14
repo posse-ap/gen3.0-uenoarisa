06efbd62655703145832c9771cc787b17ba445a6
uenoarisa <arisa-choco0326@docomo.ne.jp>
2022-08-14 23:33:46 +0900

    posse課題6週目完了

--- quiz page/script/quiz.js
+++ quiz page/script/quiz.js
@@ -1,26 +1,35 @@
 'use strict';
 {
-  for (var i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++){
     let ele = document.getElementById(`correctbox-${i}`);
     let ele1 = document.getElementById(`falsebox-${i}`);
     document.getElementById('correct-' + i).onclick = ()=>{
       if (ele1.style.display!=='block'){
         ele.style.display = 'block';
         let main = document.getElementById(`correct-${i}`)
-        main.style.backgroundColor = "red";
-        console.log(main);
+        main.style.backgroundColor = "#EFF2F5";
+        main.style.color ="#0071BC"
+        main.style.border="5px solid #0071BC"
       }
     };
 
     document.getElementById('false0-' + i).onclick = ()=>{
       if (ele.style.display!=='block'){
         ele1.style.display = 'block';
+        let main = document.getElementById(`false0-${i}`)
+        main.style.backgroundColor = "#EFF2F5";
+        main.style.color ="#0071BC"
+        main.style.border="5px solid #0071BC"
       }
     };
 
     document.getElementById('false1-' + i).onclick = ()=>{
       if (ele.style.display!=='block'){
         ele1.style.display = 'block';
+        let main = document.getElementById(`false1-${i}`)
+        main.style.backgroundColor = "#EFF2F5";
+        main.style.color ="#0071BC"
+        main.style.border="5px solid #0071BC"
       }
     };
   };
