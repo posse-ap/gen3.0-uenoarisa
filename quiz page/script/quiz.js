20e4a815f244ee869d9984cd194141ff044a19b2
uenoarisa <arisa-choco0326@docomo.ne.jp>
2022-09-01 00:26:15 +0900

    posse課題7週目完了

--- quiz page/script/quiz.js
+++ quiz page/script/quiz.js
@@ -1,54 +1,113 @@
 'use strict';
 {
-  for (let i = 0; i < 6; i++){
-    let ele = document.getElementById(`correctbox-${i}`);
-    let ele1 = document.getElementById(`falsebox-${i}`);
-    document.getElementById('correct-' + i).onclick = ()=>{
-      if (ele1.style.display!=='block'){
-        ele.style.display = 'block';
-        let main = document.getElementById(`correct-${i}`)
-        main.style.backgroundColor = "#EFF2F5";
-        main.style.color ="#0071BC"
-        main.style.border="5px solid #0071BC"
-      }
-    };
+  const ALL_QUIZ = [
+    {
+      question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？?',
+      answers: ['約28万人', '約79万人', '約183万人'],
+      correctNumber: 1,
+      note: '経済産業省 2019年3月 － IT 人材需給に関する調査',
+    },
+    {
+      question:'既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？?',
+      answers:['INTECH', 'BIZZTECH', 'X-TECH'],
+      correctNumber:2,
+    },
+    {
+      question:'IoTとは何の略でしょう?',
+      answers:['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
+      correctNumber:0,
+    },
+    {
+      question:'日本が目指すサイバー空間とフィジカル空間を高度に融合させたシステムによって開かれる未来社会のことをなんと言うでしょうか？',
+      answers:['Society 5.0', 'CyPhy', 'SDGs'],
+      correctNumber:0,
+      note:'Society5.0 - 科学技術政策 - 内閣府'
+    },
+    {
+      question:'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？?',
+      answers:['Web3.0', 'NFT', 'メタバース'],
+      correctNumber:0,
+    },
+    {
+      question:'先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
+      answers:['約2倍', '約5倍', '約11倍'],
+      correctNumber:1,
+      note:'Accenture Technology Vision 2021'
+    }
+  ];
 
-    document.getElementById('false0-' + i).onclick = ()=>{
-      if (ele.style.display!=='block'){
-        ele1.style.display = 'block';
-        let main = document.getElementById(`false0-${i}`)
-        main.style.backgroundColor = "#EFF2F5";
-        main.style.color ="#0071BC"
-        main.style.border="5px solid #0071BC"
-      }
-    };
+  const quizContainer = document.getElementById('js-quizContainer');
 
-    document.getElementById('false1-' + i).onclick = ()=>{
-      if (ele.style.display!=='block'){
-        ele1.style.display = 'block';
-        let main = document.getElementById(`false1-${i}`)
-        main.style.backgroundColor = "#EFF2F5";
-        main.style.color ="#0071BC"
-        main.style.border="5px solid #0071BC"
-      }
-    };
+  for (let i=0; i<6; i++){
+  let createQuizHtml = `<li class="js-quiz" id="content_area" data-quiz="${i}">
+  <h2 class="question">Q${i+1}</h2>
+  <p class="questiontext">${ALL_QUIZ[i].question}</p>
+  <img src="img/quiz/img-quiz0${i+1}.png" alt="">
+  <h3 class="answer">A</h3>
+  <ul class="option">
+    <li  class="option-content" id="option0-${i}" data-answer="0">${ALL_QUIZ[i].answers[0]}</li>
+    <li class="option-content" id="option1-${i}" data-answer="1">${ALL_QUIZ[i].answers[1]}</li>
+    <li class="option-content"id="option2-${i}" data-answer="2">${ALL_QUIZ[i].answers[2]}</li>
+  </ul>
+  <div class="correctbox" id="correctbox-${i}">
+    <h4>正解！</h4>
+    <p>A ${ALL_QUIZ[i].answers[ALL_QUIZ[i].correctNumber]}</p>
+  </div>
+  <div class="falsebox" id="falsebox-${i}">
+    <h4>不正解…</h4>
+    <p>A ${ALL_QUIZ[i].answers[ALL_QUIZ[i].correctNumber]}</p>
+  </div>
+  </li>`
+
+  if (ALL_QUIZ[i].note!==undefined){
+    createQuizHtml+=`<aside class="quote">
+        <img src="img/quiz/icon-note.svg" alt="">${ALL_QUIZ[i].note}
+  </aside>`;
   };
-  
+
+  quizContainer.insertAdjacentHTML('beforeend', createQuizHtml);
+  };
+
+
+  const allQuiz = document.querySelectorAll('.js-quiz');
+  allQuiz.forEach(quiz =>{
+    const answers = quiz.querySelectorAll('.option-content');
+    const selectedQuiz = quiz.getAttribute('data-quiz');
+    answers.forEach(answer => {
+      answer.addEventListener('click', ()=>{
+        console.log(selectedQuiz);
+        let selectedAnswer = answer.getAttribute('data-answer')
+        console.log(selectedAnswer);
+        console.log(ALL_QUIZ[selectedQuiz].correctNumber)
+        if (selectedAnswer==ALL_QUIZ[selectedQuiz].correctNumber ){
+          let ele = document.getElementById(`correctbox-${selectedQuiz}`);
+          ele.style.display = 'block';
+          let main = document.getElementById(`option${selectedAnswer}-${selectedQuiz}`)
+          main.style.backgroundColor = '#EFF2F5';
+          main.style.color ="#0071BC";
+          main.style.border="5px solid #0071BC";
+        }
+        else{
+          let ele1 = document.getElementById(`falsebox-${selectedQuiz}`);
+          ele1.style.display = 'block';
+          let main = document.getElementById(`option${selectedAnswer}-${selectedQuiz}`);
+          main.style.backgroundColor = '#EFF2F5';
+          main.style.color ="#0071BC";
+          main.style.border="5px solid #0071BC";
+      };
+      });
+  });
+});
 };
 
+  // const createQuizHtml = (quizItem, questionNumber) => {
+  //   const answersHtml = quizItem.answers.map((answer, answerIndex) => `<li class="p-quiz-box__answer__item">
+  //   <button class="p-quiz-box__answer__button js-answer" data-answer="${answerIndex}">
+  //     ${answer}<i class="u-icon__arrow"></i>
+  //   </button>
+  //   </li>`
+  //   ).join('');
 
-
-  // let switchBtn = document.getElementById('correct');
-  // let box = document.getElementById('correctbox');
-
-  // let changeElement = (el)=> {
-  //   //el.classList.toggle('active');
-  //   if(el.style.display=='none'){
-  //     el.style.display='';
-  //   }
+  //   const noteHtml = quizItem.note ? `<cite class="p-quiz-box__note"><i class="u-icon__note></i>${quizItem.note}
+  //   </cite>`:"";
   // }
-
-  // switchBtn.addEventListener('click', ()=> {
-  //   changeElement(box);  
-  // },false);
-
